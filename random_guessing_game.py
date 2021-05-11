15841978c3c0af1f0ed0812648c76c830737c720
MylesTrump <>
2021-05-11 19:27:25 +0000

    Cleaned up code

---
+++ random_guessing_game.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+# Created by: Myles Trump
+# Created on: May 2021
+# This program lets the user guess a number between 1-10
+#    with a randomized integer and tells them if they are correct or not
+
+
+import random
+
+
+def main():
+    # this function lets the user pick a number between 1-10
+    #   and randomizes said number
+
+    # input
+    guess = int(input("Guess an integer between 1-10: "))
+    print("")
+    randomized_number = random.randint(1, 10)  # a number between 1-10
+
+    # process & output
+    if guess == randomized_number:
+        print("You are correct")
+
+    elif guess != randomized_number:
+        print("You are not correct, the answer was {0}"
+              .format(randomized_number))
+
+    print("\nDone")
+
+
+if __name__ == "__main__":
+    main()
